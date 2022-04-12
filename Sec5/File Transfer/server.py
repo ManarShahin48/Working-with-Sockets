9683e984191037d1d3ad26672257c1f189cac398
manarshahin48 <manarshahin48@gmail.com>
2022-04-12 23:05:16 +0200

    File Transfer

---
+++ Sec5/File Transfer/server.py
@@ -0,0 +1,17 @@
+import socket
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+Host = socket.gethostname()
+Port = 1235
+s.bind((Host, Port))
+s.listen(5)
+client_socket, address = s.accept()
+print(f"Connection to {address} established")
+fileName = "test.txt"
+client_socket.send(fileName.encode('utf-8'))
+file = open("test.txt", "rb")
+data = file.read()
+client_socket.send(data)
+file.close()
+client_socket.close()
